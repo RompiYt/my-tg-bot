a68a060e8cc95e53065676f8053ea130745111fe
RompiYt <yarnikola2012@gmail.com>
2026-03-06 06:18:59 +0300

    Refactor imports and maintain functionality

--- run.py
+++ run.py
@@ -3,13 +3,13 @@ from xmlrpc import client
 from aiogram import Bot, F
 from aiogram.client.default import DefaultBotProperties
 from aiogram.types import Message
-from tgbott.config import TOKEN
-from tgbott.dispatcher import dp, router
-from tgbott.logging_setup import setup_logging
+from config import TOKEN
+from dispatcher import dp, router
+from logging_setup import setup_logging
 import logging
 from aiogram.enums import ParseMode
 import sys
-from tgbott.dispatcher import dp
+from dispatcher import dp
 from openai import AsyncClient
 
 AI_TOKEN = "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"
@@ -66,3 +66,4 @@ async def main() -> None:
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
+
